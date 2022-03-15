88f40df4f234c08578867505a81ee6c20ae14b29
Dmytro Holtsev <dmytro.holtsev@yalantis.net>
2022-03-15 16:47:33 +0200

    Remove data race in Service map access

--- service.go
+++ service.go
@@ -1,8 +1,11 @@
 package main
 
+import "sync"
+
 type Service struct {
-	subs map[int]*Subscriber
-	next int
+	subs  map[int]*Subscriber
+	next  int
+	mutex sync.Mutex
 }
 
 func NewService() *Service {
@@ -12,13 +15,17 @@ func NewService() *Service {
 }
 
 func (s *Service) Subscribe(sub *Subscriber) {
+	s.mutex.Lock()
 	s.next++
-	sub.ID = s.next
 	s.subs[sub.ID] = sub
+	s.mutex.Unlock()
+	sub.ID = s.next
 }
 
 func (s *Service) Unsubscribe(sub *Subscriber) {
+	s.mutex.Lock()
 	delete(s.subs, sub.ID)
+	s.mutex.Unlock()
 }
 
 func (s *Service) Notify(n *Notification) {
